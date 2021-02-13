1c08d35b80ee07833a10f301b9f8cab37a2b331f
log ins <iamhacker1921@gmail.com>
2021-02-13 11:37:19 +0545

    my code

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+
+    int a, b;
+     
+    printf("Enter the value of a:\n");
+    scanf("%d",&a);
+    printf("Enter the value of b:\n");
+    scanf("%d",&b);
+   int sum=a+b;
+    printf("The sum of a and b is %d",sum);
+
+
+    return 0;
+}
